373eab214580851dbee2e446e720a9b6ecdaeba6
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2022-05-04 17:09:16 +0000

    initial commit

---
+++ src/turfeth.h
@@ -0,0 +1,118 @@
+#ifndef TURFETH_H
+#define TURFETH_H
+
+// Matches ICD v0.2+.
+
+// This document details the structures specified in the TURF to SFC
+// ICD. These use bitfield unions.
+// Note: these assume struct bitfields get merged LSB to MSB.
+// There is no way to force this but if GCC ever changes it
+// pretty much everyone will raise holy hell.
+
+// note: if this bothers you from a preference standpoint,
+// just use this document as a reference and treat it as a weird
+// way to write tables of bit fields.
+
+#include <stdint.h>
+
+typedef union {
+  struct {
+    uint32_t ADDR:28;  // address to read from
+    uint32_t TAG:4;    // identifier for this request
+  } BIT;
+  uint32_t RAW;
+} turf_rdreq_t;
+
+typedef union {
+  struct {
+    uint64_t ADDR:28;     // address being acknowledged
+    uint64_t TAG:4;       // request tag this is acknowledging
+    uint64_t RDDATA:32;   // read data at this address
+  } BIT;
+  uint64_t RAW;
+} turf_rdresp_t;
+
+typedef union {
+  struct {
+    uint64_t ADDR:28;     // address to write to 
+    uint64_t TAG:4;       // identifier for this request
+    uint64_t WRDATA:32;   // data to write
+  } BIT;
+  uint64_t RAW;
+} turf_wrreq_t;
+
+typedef union {
+  struct {
+    uint64_t ADDR:28;   // address being acknowledged
+    uint64_t TAG:4;     // request tag this is acknowledging
+    uint64_t RSVD0:32;  // unused
+  } BIT;
+  uint64_t RAW;
+} turf_wrresp_t;
+
+typedef union {
+  struct {
+    uint64_t TOTAL:20;     // total length of this event (in bytes)
+    uint64_t ADDR:12;      // address of this event
+    uint64_t FRAGMENT:10;  // fragment number for this fragment
+    uint64_t KID:22;       // constant
+  } BIT;
+  uint64_t RAW;
+} turf_fraghdr_t;
+
+typedef union {
+  struct {
+    uint64_t RSVD1:20; // unused
+    uint64_t ADDR:12;  // address that was properly received
+    uint64_t TAG:8;    // identifier for this ack
+    uint64_t RSVD0:22; // unused
+    uint64_t OPEN:1;   // must be set in a *confirmation* back, otherwise,
+                       // the interface wasn't opened first
+    uint64_t ALLOW:1;  // if 1, a new event is allowed out
+  } BIT;
+  uint64_t RAW;
+} turf_ack_t;
+
+typedef union {
+  struct {
+    uint64_t TOTAL:20; // length that was not properly received
+                       // (send max if nothing received, I guess)
+    uint64_t ADDR:12;  // address that was not properly received
+    uint64_t TAG:8;    // identifier for this nack
+    uint64_t OPEN:1;   // must be set in a *confirmation* back, otherwise
+                       // the interface wasn't opened first
+    uint64_t RSVD:1;   // unused
+  } BIT;
+  uint64_t RAW;
+} turf_nack_t;
+
+#define TURF_OP_COMMAND 0x4F50
+#define TURF_CL_COMMAND 0x434C
+#define TURF_ID_COMMAND 0x4944
+#define TURF_PR_COMMAND 0x5043
+#define TURF_PW_COMMAND 0x5057
+
+typedef union {
+  struct {
+    uint64_t COMMAND:16; // 2 ASCII byte command (see above definitions)
+    uint64_t PAYLOAD:48; // payload for command
+  } BIT;
+  uint64_t RAW;
+} turf_ctl_t;
+
+// This structure describes the command to or response from the
+// PR or PW command. The PR command returns the maximum values of each
+// of these. The PW command sets the current value.
+// There's no way to read the current value because there's no point -
+// just set it to what you want.
+// The PW command has NO EFFECT is the command path is not open.
+typedef union {
+  struct {
+    uint16_t FRAGSRCMASK:16; // fragment src mask (max or current)
+    uint16_t ADDR:16;        // max addr for ack/nack
+    uint16_t FRAGMENT:16;    // fragment payload (max or current) minus 1
+  } BIT;
+  uint64_t RAW;
+} turf_ctl_param_t;
+
+#endif 
---
+++ src/turfid.c
@@ -0,0 +1,59 @@
+/** Just runs the turfid command as a test */ 
+
+
+#include "turfeth.h" 
+#include <sys/types.h> 
+#include <arpa/inet.h> 
+#include <sys/socket.h> 
+#include <stdio.h> 
+
+const char * ip = "192.168.1.128"; 
+
+int main(int nargs, char ** args) 
+{
+
+  if (nargs > 1 )
+  {
+    ip = args[1]; 
+  }
+
+  int sck = socket(AF_INET, SOCK_DGRAM, 0); 
+
+  if (sck < 0) 
+  {
+    fprintf(stderr,"Could not open socket\n"); 
+    return 1; 
+  }
+
+  turf_ctl_t idreq = {.BIT = {.COMMAND = TURF_ID_COMMAND}}; 
+
+  struct sockaddr_in dest = {.sin_family = AF_INET, .sin_port = htons(21603)}; 
+  inet_pton(AF_INET, ip, &dest.sin_addr); 
+
+  if (connect(sck, (struct sockaddr*) &dest, sizeof(dest)) < 0) 
+  {
+    fprintf(stderr,"Connection error\n"); 
+    return 1; 
+  }
+
+  if (send(sck, &idreq, sizeof(idreq),0) < 0) 
+  {
+    fprintf(stderr,"Send error...\n"); 
+    return 1; 
+  }
+
+  uint64_t repl; 
+
+  if (recv(sck, &repl, sizeof(repl),0) < sizeof(repl))
+  {
+    fprintf(stderr,"Didn't get 8 bytes...\n"); 
+    return 1; 
+  }
+
+  printf("cmd: %x, id: %llx\n", repl & 0xffff, (repl >> 16)); 
+
+  return 0; 
+
+}
+
+
