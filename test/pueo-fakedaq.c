aea682a700c7610a4bee4083278abc3217ee471b
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2025-06-20 18:56:28 +0000

    fix test program

--- test/pueo-fakedaq.c
+++ test/pueo-fakedaq.c
@@ -12,7 +12,7 @@ void handler(int signum)
   stop = 1;
 }
 
-double interval = 0.1;
+double interval = 1;
 
 int ready(pueo_daq_t * daq, uint32_t idx)
 {
@@ -21,7 +21,7 @@ int ready(pueo_daq_t * daq, uint32_t idx)
   pueo_daq_event_data_t *d = calloc(1, sizeof(pueo_daq_event_data_t) + sizeof(int16_t) * 224 * 1024);
 
   pueo_daq_get_event(daq, d, 1024);
-  char fname[128]; 
+  char fname[128];
   sprintf(fname,"/tmp/fakedaq_%05d.dat", idx);
   FILE * f  = fopen(fname,"w");
   fwrite(&d, sizeof(d), 1, f);
@@ -33,22 +33,26 @@ int ready(pueo_daq_t * daq, uint32_t idx)
 int main (int nargs, char ** args)
 {
 
-  pueo_daq_config_t cfg = { PUEO_DAQ_CONFIG_DFLT, .fragment_size = 1024 };
+  pueo_daq_config_t cfg = { PUEO_DAQ_CONFIG_DFLT, .fragment_size = 1024, .debug = true };
+
+  signal(SIGINT, handler);
 
   pueo_daq_t * daq = pueo_daq_init(&cfg);
 
+  if (!daq) return 1;
+
   pueo_daq_register_ready_callback(daq, ready);
 
-  signal(SIGINT, handler);
   pueo_daq_start(daq);
 
+  pueo_daq_dump(daq,stdout, 0);
   while(!stop)
   {
-    pueo_daq_write(daq, 0x110, 0xf00);
+    pueo_daq_soft_trig(daq);
     usleep(1e6*interval);
   }
+  pueo_daq_dump(daq,stdout, 0);
   pueo_daq_stop(daq);
-
   pueo_daq_destroy(daq);
   return 0;
 }
