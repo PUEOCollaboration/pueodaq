19c96407528305e2d11250f0452bc22b20293722
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2025-11-04 00:26:58 -0600

    fix debug output

--- src/pueodaq.c
+++ src/pueodaq.c
@@ -399,8 +399,12 @@ static int acked_multisend(pueo_daq_t * daq, int sock, uint16_t port, size_t Nse
     {
       if (daq->cfg.debug > 2)
       {
-        for (int i = 0 ; i < Nsend; i++)
-        printf(" <   0x%016lx\n", rcv.u[i]);
+        for (int i = 0 ; i < Nsend > 4 ? 4 : Nsend ; i++)
+        {
+          printf(" <   0x%016lx\n", rcv.u[i]);
+        }
+        if (Nsend > 4) printf(" ...");
+        printf("\n");
       }
       ret = 0;
       break;
