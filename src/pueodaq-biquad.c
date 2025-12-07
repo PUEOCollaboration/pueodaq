4462e6038b6fd6c832985d622fd45c907453c93d
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2025-12-07 08:04:18 +0000

    commente out debug stuff

--- src/pueodaq-biquad.c
+++ src/pueodaq-biquad.c
@@ -1,6 +1,7 @@
 #include "pueodaq.h"
 #include "pueo-biquad-coeffs.h"
 #include "daqregs.h"
+#include "pueodaq-private.h"
 
 #define BIQUAD_BASE(l, s, c, b)   ( SURF_BASE(l,s) + 0x80 * (b & 1) + 0x400 * (c & 0x7))
 
@@ -14,8 +15,11 @@ int pueo_daq_bypass_biquad(pueo_daq_t * daq, int surf_link, int surf_slot, int c
 int pueo_daq_set_biquad(pueo_daq_t *daq, int surf_link, int surf_slot, int channel, int bq_idx, const pueo_biquad_t *bq)
 {
   uint32_t base = BIQUAD_BASE(surf_link, surf_slot, channel, bq_idx);
+  //printf("USING BASE %x for SURF L%d S%d CH%d BQ%d\n", base, surf_link, surf_slot, channel, bq_idx);
+  //int old_debug = daq->cfg.debug;
+  //daq->cfg.debug = 10;
 
-  return write_based_reg(daq, base, &surf_bq.zero_fir_casc, bq->B) ||
+  int ret = write_based_reg(daq, base, &surf_bq.zero_fir_casc, bq->B) ||
   write_based_reg(daq, base, &surf_bq.zero_fir_casc, bq->A) ||
   write_based_reg(daq, base, &surf_bq.zero_fir_casc, bq->B) ||
   write_based_reg(daq, base, &surf_bq.zero_fir_casc, bq->A) ||
@@ -48,5 +52,8 @@ int pueo_daq_set_biquad(pueo_daq_t *daq, int surf_link, int surf_slot, int chann
 
   write_based_reg(daq, base, &surf_bq.update_control, 0x10001) ||
   write_based_reg(daq, base, &surf_bq.update_control, 0x810000);
+
+//  daq->cfg.debug = old_debug;
+  return ret;
 }
 
